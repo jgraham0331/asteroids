91749ed6ec21e3a8eba59da32607f30b654e50b0
Jason Graham <jgraham0331@gmail.com>
2024-09-08 13:16:00 -0400

    added clock time

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Game Loop
@@ -17,6 +20,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
